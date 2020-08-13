4fba4fea94ef2bfe614f876b7a031f3f55f9d586
folly77folly <folly77folly@gmail.com>
2020-08-13 11:06:07 +0100

    'added-otp-route'

--- otpapp/urls.py
+++ otpapp/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import  url
-from .views import LoginUser, VerifyUser, RegisterUser, VerifyToken
+from .views import LoginUser, VerifyUser, RegisterUser, VerifyToken, Booking
 from django.urls import path
 
 urlpatterns = [
     # Auth Routes
-    # path('', views.index, name ='index'),
     path('api/v.1/auth/register', RegisterUser.as_view(), name = 'register'),
     path('api/v.1/auth/verify/token', VerifyToken.as_view(), name = 'verify_token'),
     path('api/v.1/auth/verify', VerifyUser.as_view(), name = 'verify'),
     path('api/v.1/auth/login', LoginUser.as_view(), name = 'login'),
+
+    #Appointment Route
+    path('api/v.1/book/{id}', Booking.as_view(), name ='booking')
 ]
